22414beb238e2f1068ad2c51550f9f2690d53b4d
cari1928 <13030622@itcelaya.edu.mx>
2017-03-18 22:07:55 -0600

    Opcion 2 SQLite

--- app/src/main/java/com/example/radog/patm_ws_1/BDProducto.java
+++ app/src/main/java/com/example/radog/patm_ws_1/BDProducto.java
@@ -1,8 +1,12 @@
 package com.example.radog.patm_ws_1;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by radog on 16/03/2017.
@@ -10,29 +14,53 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BDProducto extends SQLiteOpenHelper {
 
-    String bd1 = "CREATE TABLE categoria(idCant INTEGER PRIMARY KEY, nomCant TEXT )";
-    String bd2 = "CREATE TABLE producto( idProd INTEGER PRIMARY KEY, nomProd TEXT, cantProd INTEGER,  idCant INTEGER, " +
-            "CONSTRAINT fk_cant FOREIGN KEY(idCant) REFERENCES categoria(idCant) )";
+    SQLiteDatabase myDB;
+
+    private static final String DB_NAME = "productos.db";
+    private static final int VERSION = 1;
+
+    private static final String TABLE_CATEGORIA = "categoria";
+    public static final String ID_CATEGORIA = "idCat";
+    public static final String NOMBRE_CATEGORIA = "nomCat";
+
+    private static final String TABLE_PRODUCTO = "producto";
+    public static final String ID_PRODUCTO = "idProd";
+    public static final String NOMBRE_PRODUCTO = "nomProd";
+    public static final String CANTIDAD_PRODUCTO = "cantProd";
+
+    private static final String CREATE_TABLE_CATEGORIA = "CREATE TABLE " + TABLE_CATEGORIA + "("
+            + ID_CATEGORIA + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + NOMBRE_CATEGORIA + " TEXT NOT NULL);";
+
+    private static final String CREATE_TABLE_PRODUCTO = "CREATE TABLE " + TABLE_PRODUCTO + "("
+            + ID_PRODUCTO + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + NOMBRE_PRODUCTO + " TEXT NOT NULL, "
+            + CANTIDAD_PRODUCTO + " INTEGER NOT NULL, "
+            + ID_CATEGORIA + " INTEGER NOT NULL, " +
+            "CONSTRAINT fk_cant FOREIGN KEY(" + ID_CATEGORIA + ") REFERENCES " + TABLE_CATEGORIA + "(" + ID_CATEGORIA + ") );";
+
+//    String bd1 = "CREATE TABLE categoria(idCant INTEGER PRIMARY KEY, nomCant TEXT )";
+//    String bd2 = "CREATE TABLE producto( idProd INTEGER PRIMARY KEY, nomProd TEXT, cantProd INTEGER,  idCant INTEGER, " +
+//            "CONSTRAINT fk_cant FOREIGN KEY(idCant) REFERENCES categoria(idCant) )";
 
     //name = nombre base de datos
-    public BDProducto(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public BDProducto(Context context) {
+        super(context, DB_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(bd1);
-        db.execSQL(bd2);
+        db.execSQL(CREATE_TABLE_CATEGORIA);
+        db.execSQL(CREATE_TABLE_PRODUCTO);
 
-
-        db.execSQL("INSERT INTO categoria(nomCant) VALUES('BEBIDAS')");
+        /*db.execSQL("INSERT INTO categoria(nomCant) VALUES('BEBIDAS')");
         db.execSQL("INSERT INTO categoria(nomCant) VALUES('LIMPIEZA')");
         db.execSQL("INSERT INTO categoria(nomCant) VALUES('SALUD Y BELLEZA')");
         db.execSQL("INSERT INTO categoria(nomCant) VALUES('CARNES FRÍAS')");
         db.execSQL("INSERT INTO categoria(nomCant) VALUES('PANADERÍA')");
         db.execSQL("INSERT INTO categoria(nomCant) VALUES('LÁCTEOS')");
         db.execSQL("INSERT INTO categoria(nomCant) VALUES('ELECTRODOMÉSTICOS')");
-        db.execSQL("INSERT INTO categoria(nomCant) VALUES('ELECTRÓNICA')");
+        db.execSQL("INSERT INTO categoria(nomCant) VALUES('ELECTRÓNICA')");*/
     }
 
     @Override
@@ -40,4 +68,36 @@ public class BDProducto extends SQLiteOpenHelper {
 
     }
 
+    public void openDB() {
+        myDB = getWritableDatabase();
+    }
+
+    public void closeDB() {
+        if (myDB != null && myDB.isOpen()) {
+            myDB.close();
+        }
+    }
+
+    public long insertCategorias(String nomCat) {
+        ContentValues objCV = new ContentValues();
+
+        objCV.put(NOMBRE_CATEGORIA, nomCat);
+
+        return myDB.insert(TABLE_CATEGORIA, null, objCV);
+    }
+
+    public long insertProducto(String nomProd, int cantProd, int idCategoria) {
+        ContentValues objCV = new ContentValues();
+
+        objCV.put(NOMBRE_PRODUCTO, nomProd);
+        objCV.put(CANTIDAD_PRODUCTO, cantProd);
+        objCV.put(ID_CATEGORIA, idCategoria);
+
+        return myDB.insert(TABLE_PRODUCTO, null, objCV);
+    }
+
+    public Cursor getAll(String table_name) {
+        String query = "SELECT * FROM " + table_name;
+        return myDB.rawQuery(query, null);
+    }
 }
--- app/src/main/java/com/example/radog/patm_ws_1/Producto.java
+++ app/src/main/java/com/example/radog/patm_ws_1/Producto.java
@@ -1,9 +1,11 @@
 package com.example.radog.patm_ws_1;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -13,28 +15,45 @@ import butterknife.OnClick;
 public class Producto extends AppCompatActivity {
 
     BDProducto objBD; //definiendo
-    SQLiteDatabase objSQL;
+    //SQLiteDatabase objSQL;
 
-    @BindView(R.id.edtNomProd)
-    EditText edtNomProd;
-    @BindView(R.id.edtCantProd)
-    EditText edtCantProd;
+    @BindView(R.id.edtNomProd) EditText edtNomProd;
+    @BindView(R.id.edtCantProd) EditText edtCantProd;
+    @BindView(R.id.tvMsg) TextView tvMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
-
         ButterKnife.bind(this);
 
-        objBD = new BDProducto(this, "BDPRODUCTOS", null, 1);
-        objSQL = objBD.getWritableDatabase();
-        Toast.makeText(this, "Se insertaron categorias", Toast.LENGTH_LONG).show();
+        objBD = new BDProducto(this);
+        //objSQL = objBD.getWritableDatabase();
+        //Toast.makeText(this, "Se insertaron categorias", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        objBD.openDB();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        objBD.closeDB();
     }
 
     @OnClick(R.id.btnSave)
     public void btnSave() {
-        try {
+        long resultado = objBD.insertProducto(getValue(edtNomProd), Integer.parseInt(getValue(edtCantProd)), 1);
+        if (resultado == -1) {
+            Toast.makeText(this, "Some error ocurred while inserting", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Data inserted successfully, ID " + resultado, Toast.LENGTH_SHORT).show();
+        }
+
+        /*try {
             String nomProd = edtNomProd.getText().toString();
             int cantProd = Integer.parseInt(edtCantProd.getText().toString());
 
@@ -47,9 +66,24 @@ public class Producto extends AppCompatActivity {
             Toast.makeText(this, "Se insertó el producto", Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-
-
+        }*/
     }
 
+    @OnClick(R.id.btnShow)
+    public void btnShow() {
+        StringBuffer finalData = new StringBuffer();
+        Cursor objC = objBD.getAll("categoria");
+        for (objC.moveToFirst(); !objC.isAfterLast(); objC.moveToNext()) {
+            finalData.append(objC.getInt(objC.getColumnIndex(objBD.ID_CATEGORIA)));
+            finalData.append(" - ");
+            finalData.append(objC.getString(objC.getColumnIndex(objBD.NOMBRE_CATEGORIA)));
+            finalData.append("\n");
+        }
+
+        tvMsg.setText(finalData);
+    }
+
+    private String getValue(EditText et) {
+        return et.getText().toString().trim();
+    }
 }
